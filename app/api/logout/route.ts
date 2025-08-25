7233089d41cc7ed2454ed9eec7a4c4f52dbab2f4
AdamDevelop74 <adam-develop@gmx.de>
2025-08-25 17:43:16 +0200

    Update route.ts

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,12 +1,14 @@
 // /app/api/logout/route.ts
-
+import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
-//import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'  // falls genutzt
 
 export async function POST(req: Request) {
   // Supabase bereit machen (TypeScript)
-  //const supabase = createServerComponentClient({ req, res: null })
-  //await supabase.auth.signOut()
+  const supabase = createServerComponentClient({ cookies: () => cookies()  })
+
+  await supabase.auth.signOut()
 
   return NextResponse.redirect(new URL('/login', req.url))
 }
+
